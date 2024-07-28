4d25ef9ead28f49314a4b95fd151a5d6e26b1f2d
rekha <mourrekha94@gmail.com>
2024-07-28 19:22:45 +0100

    Initial commit

---
+++ src/Lec30/Stack_insert_at_bottom.java
@@ -0,0 +1,18 @@
+package Lec30;
+
+import java.util.Stack;
+
+public class Stack_insert_at_bottom {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println(st);
+        System.out.println(st);
+    }
+}
